c712bef7ffd6cb608a5d63a6995fd4afa184c212
12Dreflections <julianadamwise@gmail.com>
2016-12-24 22:28:08 +1100

    First Tensor

---
+++ a_running_tf/run_tf.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+# Set up the variables
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+
+result = tf.mul(x1,x2)
+
+print(result)
+
+# Run the session
+with tf.Session() as sess:
+	print(sess.run(result))
