5b801016967ca55cc3a32b67b33a3cd4227c9353
12Dreflections <julianadamwise@gmail.com>
2016-12-28 12:00:57 +1100

    Create a model from word vectors

---
+++ c_own_data/nn_own_data.py
@@ -0,0 +1,97 @@
+import nltk 
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer #lemmatizer ensures 'nltk.stem' stems to full words 
+import numpy as np
+import random
+import pickle
+from collections import Counter
+
+lemmatizer = WordNetLemmatizer()
+hm_lines = 100000000
+
+'''
+Create the lexicon list of language, from 'Pos' and 'Neg' files
+'''
+def create_lexicon(pos,neg):
+	lexicon = []
+
+	for fi in [pos,neg]: # collect all lexicon from file
+		with open(fi, 'r') as f:
+			contents = f.readlines()
+			for l in contents[:hm_lines]:
+				all_words = word_tokenize(l.lower())
+				lexicon += list(all_words)
+
+	lexicon = [lemmatizer.lemmatize(i) for i in lexicon] #reduce word lexicon to lemmatized
+	w_counts = Counter(lexicon) # dict of word frequency  {'the:5252, 'and':434, ...}
+	
+	l2 = [] # l2 is final lexicon
+	for w in w_counts:
+		#print(w_counts[w])
+		if 1000 > w_counts[w] > 50: # filter out words like 'the, and, of'
+			l2.append(w)
+	print('My language l2 is length: ', len(l2))
+	return l2
+
+'''
+Return classified samples, as a list of the language with [1,0] as [pos, neg] classification
+'''
+def sample_handling(sample, lexicon, classification):
+
+	featureset = []
+
+	'''
+					    features      pos neg	
+		featureset = [
+						[1 0 0 1 1 0], [1 0],
+						[0 1 0 1 0 0], [0 1],
+						 ...
+					  ]
+	'''
+
+	with open(sample, 'r') as f:
+		contents = f.readlines()
+		for l in contents[:hm_lines]:
+			current_words = word_tokenize(l.lower())
+			current_words = [lemmatizer.lemmatize(i) for i in current_words]
+			features = np.zeros(len(lexicon))
+			for word in current_words:
+				if word.lower() in lexicon:
+					index_value = lexicon.index(word.lower())
+					features[index_value] += 1
+			features = list(features)
+			featureset.append([features, classification])
+
+	return featureset
+
+# Assign positive and negative to text files, 
+def create_feature_sets_and_labels(pos,neg,test_size=0.1):
+	lexicon = create_lexicon(pos, neg)
+	features = []
+	features += sample_handling('pos.txt', lexicon, [1,0]) # positive is 1 0, negative is 0 1
+	features += sample_handling('neg.txt', lexicon, [0,1])
+
+	random.shuffle(features)
+	features = np.array(features)
+
+	testing_size = int(test_size*len(features))
+
+	# Train on first 10% of testing_size, test on remainder 90%  
+	train_x = list(features[:,0][:-testing_size])  # numpy trick [:,0] take the 'zero'th element' of multi list list, to take from the feature nodes list and nto the pos/neg list
+	train_y = list(features[:,1][:-testing_size])  
+
+	test_x = list(features[:,0][:-testing_size:])  
+	test_y = list(features[:,1][:-testing_size:])  
+
+
+	train_x = list(features[:,0][:-testing_size])
+	train_y = list(features[:,1][:-testing_size])
+	test_x = list(features[:,0][-testing_size:])
+	test_y = list(features[:,1][-testing_size:])
+
+	return train_x, train_y, test_x, test_y
+
+if __name__=='__main__':
+	train_x, train_y, test_x, test_y = create_feature_sets_and_labels('pos.txt', 'neg.txt')
+	with open('sentiment_set.pickle', 'wb') as f:
+		pickle.dump([train_x, train_y, test_x, test_y], f)
---
+++ c_own_data/readme.md
@@ -0,0 +1,23 @@
+
+This example uses data imported from NLTK
+
+# Dependencies
+
+To install NLTK
+---------------
+pip3 install nltk
+
+python3
+import nltk
+nltk.download('all', halt_on_error=False)
+press 'n' on any it cannot access to continue downloading the rest
+d
+all
+
+
+# Usage
+
+### Train NN Model  
+run nn_own_data.py to train and pickle a neural network model,
+Saves a pickled model from the Neural Network trained from a Positive and Negatively classified input data. 
+
